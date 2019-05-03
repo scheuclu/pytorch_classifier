9c90b8ceee9a104c9c9a1bfc406d9f972a5b978b
Lukas Scheucher <lukas.scheucher@volkswagen@volkswagen.de>
2019-05-03 11:50:48 +0200

    Refactored evaluation

--- eval.py
+++ eval.py
@@ -1,7 +1,7 @@
 
 import torch
-from torchvision import datasets, models, transforms
-from mydataset import MyDataset
+from torchvision import models
+from deprecated.mydataset import MyDataset
 import pandas as pd
 
 import py3nvml
@@ -12,7 +12,12 @@ pickle_file = '/raid/user-data/lscheucher/projects/bounding_box_classifier/full_
 from collections import namedtuple
 IndexEntry = namedtuple('IndexEntry', ['img_path', 'sub_idx', 'classname', 'left', 'top', 'right', 'bottom'], verbose=False)
 from easydict import EasyDict as edict
-from configs import configs, index2name, name2index
+from configs import configs
+import torch.nn as nn
+from tqdm import tqdm as tqdm
+import os
+
+from dataset_with_paths import ImageFolderWithPaths
 
 """ Parse argv --------------------------------------------------------------"""
 import argparse
@@ -21,7 +26,7 @@ parser = argparse.ArgumentParser(description='Train pytorch classifier.')
 
 parser.add_argument('--config',
                     type=str,
-                    default='debug',
+                    default='resnet_set_0',
                     help='Name of configuration')
 
 parser.add_argument('--port',
@@ -31,8 +36,14 @@ parser.add_argument('--port',
 
 parser.add_argument('--checkpoint',
                     type=str,
-                    default='./checkoints/long_train_1.0',
+                    default='/raid/user-data/lscheucher/tmp/pytorch_classifier/checkpoints/resnet_set_0.498',
                     help='Path to the checkpoint')
+
+parser.add_argument('--resultfile',
+                    type=str,
+                    default='/raid/user-data/lscheucher/tmp/pytorch_classifier_data/results/res8',
+                    help='Path to the checkpoint')
+
 
 args = parser.parse_args()
 opts = edict(configs[args.config])
@@ -40,19 +51,62 @@ opts = edict(configs[args.config])
 
 
 """ Load model from path """
-PATH = './checkpoints/test1.2'
-model = models.squeezenet.SqueezeNet(num_classes=5)
-model.load_state_dict(torch.load(PATH))
+
+""" Model, Optimizer, Scheduler"""
+if opts.model == 'SqueezeNet':
+
+    model = models.squeezenet1_0(pretrained=True)
+    model.fc = nn.Linear(512, 5, bias=True)
+
+elif opts.model == 'ResNet18':
+
+    model = models.resnet18(pretrained=True)
+    model.fc = nn.Linear(512, 5, bias=True)
+elif opts.model == 'DenseNet169':
+
+    model = models.resnet18(pretrained=True)
+    model.fc = nn.Linear(512, 5, bias=True)
+else:
+    raise ValueError("model not supported",opts.model)
+
+model.load_state_dict(torch.load(args.checkpoint))
 model.eval()
 model = model.to(device)
 
 
 """ Create validation dataloader """
-dataset_val   = MyDataset(pickle_file=pickle_file, mode='val')
-dataset_loader_val = torch.utils.data.DataLoader(dataset_val,
-                                             batch_size=100, shuffle=True,
-                                             num_workers=40)
+from torchvision import datasets, models, transforms
 
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+eval_dir = '/raid/user-data/lscheucher/tmp/pytorch_classifier_data/val0'
+eval_dataset = ImageFolderWithPaths(
+    eval_dir,
+    transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+        transforms.ToTensor(),
+        normalize,
+    ]))
+
+
+eval_sampler = torch.utils.data.SequentialSampler(eval_dataset)
+
+eval_loader = torch.utils.data.DataLoader(
+    eval_dataset, batch_size=opts.batchsize, shuffle=False,
+    num_workers=20, pin_memory=True, sampler=eval_sampler)
+
+class2idx = eval_dataset.class_to_idx
+idx2class = {val:key for key,val in class2idx.items()}
+
+"""
+tset = eval_dataset.imgs
+len(tset)
+Out[11]: 232364
+
+
+"""
 
 
 preds = {
@@ -68,51 +122,36 @@ preds = {
 
 import random
 random.seed(0);
-idx_list = random.sample(range(len(dataset_val.data)),200)
-for idx in idx_list:
-#for idx in range(len(dataset_val)):
-    entry, img, label = dataset_val.get_item_eval(idx)
-    print("Label:", label)
+idx_list = random.sample(range(len(eval_dataset)), 200)
 
-    img = img.to(device)
 
-    pred = model.forward(img[None, :, :, :])
+all_paths = []
+all_classnames = []
+all_pred_classnames = []
 
+for inputs, labels, paths  in tqdm(eval_loader):
+
+    #print(1)
+    inputs, labels= inputs.to(device), labels.to(device)
+    #print(2)
+    pred = model.forward(inputs)
+    #print(3)
     prediction, index = pred.max(1)
-    #tensor([[0.0000, 0.5754, 0.0000, 0.0239, 0.0000]], grad_fn= < ViewBackward >)
-
+    #print(4)
     #store the eval results
-    pred_classname = index2name[int(index)]
-    preds['pred_classname'].append(pred_classname)
-    for key, val in entry.items():
-        preds[key].append(val)
-    print(idx)
+    pred_classnames = [idx2class[int(idx)] for idx in index]
+    classnames      = [idx2class[int(idx)] for idx in labels]
 
+    all_pred_classnames+=pred_classnames
+    all_classnames+=classnames
+    all_paths+=paths
+    #break
 
-from collections import Counter
-eval_stat = pd.DataFrame.from_dict(preds)
-print(preds)
-print("Success")
+val_annotations = pd.DataFrame(index=all_paths, columns= ['classname', 'pred_classname'], data= [[a,b] for a,b in zip(all_classnames, pred_classnames)])
+checkpoint_dir, checkpoint_name = args.checkpoint.rsplit('/', maxsplit=1)
+result_file_path = os.path.join(checkpoint_dir,'val_'+checkpoint_name+'.csv')
 
-print("Counter ground truth classes:")
-print(Counter(eval_stat.classname))
+val_annotations.to_csv(result_file_path)
+print("Wrote results to", result_file_path)
 
-print("Counter prediction classes:")
-print(Counter(eval_stat.pred_classname))
-
-
-
-""" Evaluate classifications class-wise """
-for classname in name2index.keys():
-    num_correct_preds = sum((eval_stat.classname==classname) & (eval_stat.pred_classname == classname))
-    num_wrong_preds = sum((eval_stat.classname == classname) & (eval_stat.pred_classname != classname))
-    print(classname, num_correct_preds, num_wrong_preds)
-
-
-"""
-test = next(iter(dataset_loader_val))
-test[0].shape
-Out[6]: torch.Size([100, 3, 224, 224])
-test[1].shape
-Out[7]: torch.Size([100])
-"""
+print("Done")
